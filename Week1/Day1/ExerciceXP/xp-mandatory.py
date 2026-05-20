c50e66f497ef399413ef0b026fe33fccaf9a76a7
LEBEST0 <angedavidakre33@gmail.com>
2026-05-20 00:15:13 +0000

    je me suis trompé j'ai tout commenté

--- Week1/Day1/ExerciceXP/xp-mandatory.py
+++ Week1/Day1/ExerciceXP/xp-mandatory.py
@@ -1,16 +1,16 @@
 # Exercice 1: Hello World
-"""
+
 print("Hello world \n"*4 )
-"""
+
 
 # Exercice 2: Some Math
 
-"""
+
 print((99**3)*8)
-"""
+
 # Exercice 3: What is the output?
 
-"""
+
 
 print(15 < 8)  # False
 print(5 < 3)  # False
@@ -18,29 +18,25 @@ print(3 == 3) # True
 print(3 == "3") # False
 print("3" > 3)   # ErrorType
 print("Hello" == "hello") #False
-"""
 
 # Exercice 4: Your computer brand
 
-"""
 computer_brand= "HP"
 print(f"I have a {computer_brand} computer.")
-"""
 
 # Exercice 5: Your information
 
-"""
+
 name="AKRE ANGE DAVID"
 age=22
 shoe_size=42
 info=f"Hello, my name's {name}, I'm {age} years old. I love technology and wear shoe with size {shoe_size} "
 
 print(info)
-"""
+
 
 # Exercise 6: A & B
 
-"""
 
 a=1
 b=2
@@ -48,25 +44,22 @@ b=2
 if a>b:
     print("Hello World") 
     
-"""
+
 
 
 # Exercise 7: Odd or Even
 
-"""
+
 
 numb=int(input("Enter your number: "))
 if numb%2==0:
     print("This number is even")
 else: 
-    print("This number is odd")"""
+    print("This number is odd")
 
 
 # Exercice 8 : What’s your name?
 
-
-
-"""
 mon_nom = "AKRE" 
 nom = input("Enter you name : ")
 
@@ -74,7 +67,7 @@ if nom.lower() == mon_nom.lower():
     print("No way ! We have the same first name, are we twins or ? 😂")
 else:
     print(f"Ah {nom}... Pas aussi bien que {mon_nom} comme prénom...")
-"""
+
 
 # Exercice  9:  Tall enough to ride a roller coaster
 
